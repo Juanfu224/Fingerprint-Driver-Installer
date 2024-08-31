c3d95b94882b2b72705e2a6ef5666921707a02a3
Juanfu224 <88267143+Juanfu224@users.noreply.github.com>
2024-08-31 02:46:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import os
 
+# Update the system
+os.system("sudo apt update")
+
 # Removes the fprintd and libpam-fprintd packages from the system, along with their configuration files.
-os.system("sudo apt purge fprintd libpam-fprintd")
+os.system("sudo apt purge fprintd libpam-fprintd -y")
 
 # Installs the necessary tools and dependencies required to compile and work with libfprint.
-os.system("sudo apt install meson cmake libglib2.0-dev libgusb-dev libcairo2-dev libgirepository1.0-dev libnss3-dev libgudev-1.0-dev gtk-doc-tools valgrind")
+os.system("sudo apt install meson cmake libglib2.0-dev libgusb-dev libcairo2-dev libgirepository1.0-dev libnss3-dev libgudev-1.0-dev gtk-doc-tools valgrind -y")
 
 # Clones the official libfprint repository.
 os.system("git clone https://gitlab.freedesktop.org/libfprint/libfprint.git")
 
 # Copies the cloned libfprint directory to the current working directory.
-os.system("sudo cp -r libfprint .")
+os.system("sudo cp -r libfprint/* .")
 
 # Configures the build of the libfprint project in a directory named 'builddir'.
 os.system("sudo meson builddir")
@@ -19,7 +22,7 @@ os.system("sudo meson builddir")
 os.system("sudo meson install -C builddir")
 
 # Reinstalls the fprintd and libpam-fprintd packages, which are needed for fingerprint authentication.
-os.system("sudo apt install fprintd libpam-fprintd")
+os.system("sudo apt install fprintd libpam-fprintd -y")
 
 # Updates the system's authentication configuration to apply changes to PAM (Pluggable Authentication Modules).
 os.system("sudo pam-auth-update")
